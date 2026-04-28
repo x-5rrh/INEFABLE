f2700a93b50238e54e0cdbb361ed258948ec6f38
x-5rrh <nahuelsrusso@gmail.com>
2026-04-28 15:29:44 -0300

    Estructura del tp hecha con codeblocks
    falta añadir la gbt

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Apellido: Russo Rodriguez, Nahuel Sebastian
+DNI: 43820382
+Usuario: x-5rrh
+Entrega: Sí
+
+Apellido: Blanco, Priscila
+DNI: 44355393
+Usuario: PriWhite
+Entrega: Sí
+
+Apellido: Benito, Pablo
+DNI: 43569575
+Usuario: PabloBeni
+Entrega: Sí
+*/
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
